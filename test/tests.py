5049c8276ab77fdcb6bc01cc19c113f44693b4db
julian piedrahita <julianpiedrahita@MacBook-Pro-de-julian.local>
2024-01-18 11:50:54 -0500

    test de test pytest

--- test/tests.py
+++ test/tests.py
@@ -2,9 +2,7 @@ import random
 import pytest
 
 
-@pytest.mark.skip(reason="don't run this test")
-def test_skipped():
-    assert "pytest-emoji" != ""
+
 
 
 @pytest.mark.parametrize(
@@ -16,6 +14,8 @@ def test_skipped():
     ],
 )
 
+def test_passed(name, expected):
+    assert f"Hello {name}!" == expected
 
 
 def test_error(number):
